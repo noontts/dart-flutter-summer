da74597efc5137b87f7bfee54bc3d75e198c1e86
thitisan.k <thitisan.k@bluebik.com>
2024-04-29 19:51:52 +0700

    fix: improve jumbotron

--- widget_compose/lib/widget/compound/jumbotron/home_jumbotron.dart
+++ widget_compose/lib/widget/compound/jumbotron/home_jumbotron.dart
@@ -8,29 +8,25 @@ class HomeJumbotron extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 250,
-      width: MediaQuery.of(context).size.width,
       child: Stack(
-        children: [
+        children: <Widget>[
           Image.network(
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
-              'https://images.unsplash.com/photo-1714165860646-b8de17af3bc8?q=80&w=2969&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      BigText(title: 'OUTERWEAR'),
-                      PrimaryButton(title: 'View Collection', titleColor: Colors.white)
-                    ],
-                  ),
-                )
-              ],
+            'https://images.unsplash.com/photo-1714165860646-b8de17af3bc8?q=80&w=2969&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 260,
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    BigText(title: 'New Collection'),
+                    PrimaryButton(title: 'View Collection')
+                  ],
+                ),
+              ),
             ),
           )
         ],
